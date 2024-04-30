a7f54b8888155cef16b59a3d2aa347b53b5548cd
ilathem <isaiahlathem@gmail.com>
2024-04-30 06:28:08 -0500

    create initial card

---
+++ src/card.js
@@ -0,0 +1,3 @@
+export default function Card(data) {
+  const container = document.createElement('div');
+}
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ document.body.addEventListener('keyup', (e) => {
 
 const getWeather = async () => {
   try {
-    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=ca55ec8337974141a05233215242104&q=${document.querySelector('#location-input').value}`)
+    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=ca55ec8337974141a05233215242104&q=${document.querySelector('#location-input').value}&days=3`)
     const data = await response.json();
     console.log(data);
   } catch (error) {
