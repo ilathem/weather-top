d586b774ba49f2647598ee093a8ef5891c077486
ilathem <isaiahlathem@gmail.com>
2024-04-30 06:18:44 -0500

    Add initial queries to weather api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-import "./index.css";
+import './index.css';
+
+document.body.addEventListener('keyup', (e) => {
+  if (e.key === 'Enter') {
+    console.log('enter pressed');
+    getWeather()
+  }
+});
+
+const getWeather = async () => {
+  try {
+    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=ca55ec8337974141a05233215242104&q=${document.querySelector('#location-input').value}`)
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error)
+  }
+}
